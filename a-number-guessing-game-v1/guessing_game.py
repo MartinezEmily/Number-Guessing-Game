6e20316d85671e950ae15c2c43d8c2d648e052ab
MartinezEmily <Emily.Martinez039@gmail.com>
2025-02-17 18:58:12 -0500

    Defined functions and created while loops

--- a-number-guessing-game-v1/guessing_game.py
+++ a-number-guessing-game-v1/guessing_game.py
@@ -1,13 +1,61 @@
-"""
-Data Analysis Techdegree
-Project 1 - A Number Guessing Game
---------------------------------
-"""
-
-# Import the random and statistics modules.
+import random 
+import statistics
+import sys
 
 # Create the start_game function.
 # Write your code inside this function.
+
+def game_instructions():
+    print("""The game will randomly select a number between 1 to 10. 
+Your goal is to guess the number with the fewest attempts.
+After each guess, you will receive feedback:
+    - 'Too low!' if your guess is smaller than the number.
+    - 'Too high!' if your guess is greater than the number.
+    - 'Correct!' if you guessed the right number!
+You can quit anytime by typing 'QUIT'.
+""")
+# starts the game
+def play_game():
+    random_number = random.randint(1, 10)
+    attempt = []
+
+    while True:
+        try:
+            user_guess = int(input("Guess a number between 1 and 10:   "))
+            if user_guess > random_number:
+                print("Too high!")
+
+            elif user_guess < random_number:
+                print("Too low!")
+            else: 
+                print("Correct!")
+                break
+        except ValueError:
+            print("Please enter a valid number.") 
+
+# kicks off the app with a menu
+def start_game():
+    print("""\N{PARTY POPPER} Welcome to the Number Guessing Game! \N{PARTY POPPER}
+Type 'INSTRUCTIONS' to learn how to play.
+Type 'START' to play the game.
+Type 'QUIT' to exit the game.
+""")
+
+    while True:
+        user_input = input("> ")
+
+        if user_input.upper() == 'INSTRUCTIONS':
+            game_instructions()
+        elif user_input.upper() == 'START':
+            play_game()
+        elif user_input.upper() == "QUIT":
+            print("Thanks for playing! \N{WAVING HAND SIGN}")
+            break
+        else:
+            print("Invalid choice. Type 'INSTRUCTIONS', 'START' or 'QUIT'")
+
+
+
 
 #   When the program starts, we want to:
 #   ------------------------------------
@@ -31,3 +79,4 @@ Project 1 - A Number Guessing Game
 
 
 # Kick off the program by calling the start_game function.
+start_game()
